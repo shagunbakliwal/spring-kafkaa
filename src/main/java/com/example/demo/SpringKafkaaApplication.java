18f70f2f35fd799d91b12c65fde54037d4fc665e
Shagun Bakliwal <shagunbakliwal@gmail.com>
2019-03-08 09:40:11 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/SpringKafkaaApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaaApplication 
+///implements ApplicationRunner 
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaaApplication.class, args);
+	}
+
+}
